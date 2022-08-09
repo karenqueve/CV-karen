d51b919b5c9a173ba26e9f67760f950272a4151b
karen-yquevedo <kyamilequevedo@gmail.com>
2022-08-09 14:10:45 +0200

    header works

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Header from './Header';
+import Header from './components/Header';
 import './App.css';
+import About from './components/About';
+import Technologies from './components/Technologies';
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
+      <Technologies />
     </div>
   );
 }
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Technologies = () => {
+    return (
+        <div>
+            <h1>Estoy dentro de Technologies</h1>
+        </div>
+    )
+}
+
+export default Technologies
