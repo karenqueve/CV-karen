830670b29d27efb3f1806c7f48f1822fa3a925ba
karen-yquevedo <kyamilequevedo@gmail.com>
2022-08-15 13:56:26 +0200

    archivo cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Header from "./components/Header";
 import './App.css';
 import { useState } from "react";
+import Header from "./components/Header";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+
 
 function App() {
 
@@ -10,11 +11,12 @@ function App() {
   return (
 
     <div>
-      <button onClick={() => setShow("about")}>About</button>
-      {show === 'about' ? <About /> : null}
-      <button onClick={() => setShow("technologies")}>Technologies</button>
-      {show === 'technologies' ? <Technologies /> : null}
       <Header />
+      <button onClick={() => setShow("about")}>About</button>
+      <button onClick={() => setShow("technologies")}>Technologies</button>
+      {show === 'about' ? <About /> : null}
+      {show === 'technologies' ? <Technologies /> : null}
+
     </div>
 
   );
