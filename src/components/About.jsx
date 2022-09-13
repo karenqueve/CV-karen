38afaca80180acd1f5c749f296e417a8b3ce8ef3
karen-yquevedo <kyamilequevedo@gmail.com>
2022-09-13 23:19:00 +0200

    img rota

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-
 export default function About({ about }) {
     return (
         <div className='title'>
             <h1>Estoy dentro de About</h1>
 
             <h2>{about.name}</h2>
-            <img src={require('../assets/is_my.jpg')} alt='' />
+            <div className='cardImg'> <img src={require('../assets/is_my.jpg')} alt='' />
+            </div>
         </div>
     )
 }
+
